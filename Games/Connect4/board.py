359b04963becfed4cc49e6dcbddbdb7f1fa93f0e
matthewfaler <matthewfaler1@gmail.com>
2025-10-29 18:22:35 -0400

    #13: Decoupled user input from turn logic.

---
+++ Games/Connect4/board.py
@@ -0,0 +1,72 @@
+import numpy
+
+class Board:
+ 
+    colors = ('●', '○')
+    below = 6
+    right = 7
+    left = -1
+    above = -1
+    
+    def __init__(self):
+        self.board = numpy.full([self.below, self.right], '·', dtype='U1')
+        self.player = 0 
+        self.nextSpots = [5, 5, 5, 5, 5, 5, 5]
+
+    def toggleColor(self):
+        self.player = 1 - self.player
+
+    def put(self, x: int):
+        if self.nextSpots[x] == self.above:
+            print("\033[31mError: Column is full.\033[0m")
+            return False
+        self.board[self.nextSpots[x]][x] = self.colors[self.player]
+        if self.checkVictory(x, self.nextSpots[x]):
+            return True
+        self.toggleColor();
+        self.nextSpots[x] -= 1
+        return False
+
+    def checkVictory(self, x: int, y: int):
+        hor, ver, diag, adiag = 1, 1, 1, 1
+        connect = 4
+        for i in range(1, connect):
+            if y + i >= self.below or self.board[y + i][x] != self.colors[self.player]: break
+            else: ver += 1
+        if ver >= connect: return True
+
+        for i in range(1, connect):
+            if x + i >= self.right or self.board[y][x + i] != self.colors[self.player]: break
+            else: hor += 1
+        if hor >= connect: return True
+        for i in range(1, connect):
+            if x - i <= self.left or self.board[y][x - i] != self.colors[self.player]: break
+            else: hor += 1
+        if hor >= connect: return True
+
+        for i in range(1, connect):
+            if x - i <= self.left or y - i <= self.above or self.board[y - i][x - i] != self.colors[self.player]: break
+            else: diag += 1
+        if diag >= connect: return True
+        for i in range(1, connect):
+            if x + i >= self.right or y + i >= self.below or self.board[y + i][x + i] != self.colors[self.player]: break
+            else: diag += 1
+        if diag >= connect: return True
+
+        for i in range(1, connect):
+            if x - i <= self.left or y + i >= self.below or self.board[y + i][x - i] != self.colors[self.player]: break
+            else: adiag += 1
+        if adiag >= connect: return True
+        for i in range(1, connect):
+            if x + i >= self.right or y - i <= self.above or self.board[y - i][x + i] != self.colors[self.player]: break
+            else: adiag += 1
+        if adiag >= connect: return True
+
+        return False
+
+    def checkFull(self):
+       return all(x == -1 for x in self.nextSpots)
+
+    def out(self):
+        print("  A  B  C  D  E  F  G")
+        print(numpy.array2string(self.board, separator="  ", formatter={'all': lambda x: x}).replace("[", " ").replace("]", ""))     # type: ignore
---
+++ Games/Connect4/game.py
@@ -0,0 +1,36 @@
+import board
+
+class Game:
+    def __init__(self):
+        self.board = board.Board()
+        self.finished = False
+
+    def promptPlayer(self):
+        inp = input("Choose which column to play (A-G): ").upper()
+        colmap = {'A':0,'B':1,'C':2,'D':3,'E':4,'F':5,'G':6}
+
+        if inp not in colmap:
+            print("Please choose a valid option.")
+            return
+        
+        if self.board.nextSpots[colmap[inp]] == self.board.above:
+            print("\033[31mError: Column is full.\033[0m")
+
+        self.turn(colmap[inp])
+
+    def turn(self, move):
+        won = self.board.put(move)
+        if won:
+            self.board.out()
+            print(f"Player {self.board.colors[self.board.player]} wins!")
+            self.finished = True
+        elif self.board.checkFull():
+            self.board.out()
+            print("It's a draw!")
+            self.finished = True
+
+if __name__ == "__main__":
+    g = Game()
+    while not g.finished:
+        g.board.out()
+        g.promptPlayer()
